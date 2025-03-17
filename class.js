28f8172f55cb7deb9f146e74a25d5124f6590aea
Pietrosor <pietrosorbo@hotmail.it>
2025-03-17 16:49:02 +0100

    Forms

---
+++ class.js
@@ -0,0 +1,29 @@
+const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
+const age = document.getElementById("age")
+const address = document.getElementById("address")
+
+class User {
+  constructor(_firstName, _lastName, _age, _address) {
+    this.firstName = _firstName
+    this.lastName = _lastName
+    this.age = _age
+    this.address = _address
+  }
+}
+
+const form = document.getElementById("userForm")
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+  console.log("INVIO FORM")
+  const user1 = new User(
+    firstName.value,
+    lastName.value,
+    age.value,
+    address.value
+  )
+  console.log("CONTATTO SALVATO ", user1)
+  form.reset()
+})
+
+const ageDifference = function (otherUser) {}
---
+++ pets.js
@@ -0,0 +1,28 @@
+const petNameInput = document.getElementById("petName")
+const ownerNameInput = document.getElementById("ownerName")
+const speciesInput = document.getElementById("species")
+const breedInput = document.getElementById("breed")
+
+class Pet {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petNameInput = _petName
+    this.ownerNameInput = _ownerName
+    this.speciesInput = _species
+    this.breedInput = _breed
+  }
+}
+
+const form = document.getElementById("petForm")
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+  console.log("INVIO FORM")
+
+  const anotherPet = new Pet(
+    petNameInput.value,
+    ownerNameInput.value,
+    speciesInput.value,
+    breedInput.value
+  )
+  console.log("Nuovo animale" + anotherPet)
+  form.reset()
+})
